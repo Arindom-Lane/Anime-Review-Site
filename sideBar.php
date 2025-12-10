1bc2bdc94521ca34fe2a267ff1d59131a1f1f2f2
arindom <arindom793@gmail.com>
2025-12-10 07:12:58 +0600

    uisng google icon

--- sideBar.php
+++ sideBar.php
@@ -3,12 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"rel="stylesheet">
     <title>Document</title>
 
 
 </head>
 <body>
+<div class="container">
+    <aside>
+        <div class="top">
+            <div class="logo">
+                <img src="download.png">
+                <h2>My <span class="danger">AnimeList</span></h2>
+            </div>
+            <div class="close" id="close-Btn">
+                <span class="material-symbols-rounded">
+                    close
+                </span>
 
+            </div>
+        </div>
+
+
+
+    </aside>
+
+</div>
 
     
 </body>
