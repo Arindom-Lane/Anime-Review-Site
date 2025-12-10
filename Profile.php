29078789e6333de613e4c9b3889fda5cb5318d1c
arindom <arindom793@gmail.com>
2025-12-10 19:56:27 +0600

    /\.

---
+++ Profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet">
+    <title>My AnimeList Dashboard</title>
+    <link rel="stylesheet" href="style.css">    
+    
+</head>
+<body>
+    <header>
+        
+        <div class="topbar-right">
+            <img src="download.png" id="logo">
+            <span class="material-symbols-rounded icon menu">menu</span>
+            <span class="material-symbols-rounded icon mail">mail</span>
+        
+            <span class="material-symbols-rounded icon bell">notifications</span>
+            <span class="badge">1</span>
+        <div class="profile">
+            <img src="your-image.png" alt="">
+            <span class="username">Arino_99</span>
+        </div>
+        </div>
+    
+
+    </header>
+
+    
+    
+</body>
+</html>
