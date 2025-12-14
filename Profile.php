8f80c8e9acb841ccce4d312c710d18a1d8351ae7
Arindom-Lane <arindom793@gmail.com>
2025-12-14 04:17:32 -0800

    added vertical scroll panel

--- Profile.php
+++ Profile.php
@@ -41,7 +41,30 @@
 
     <main>
         <div class="leftSection">
-            <div>HIii</div>
+            <div class="fallList">
+                <span class="left-arrow"><</span>
+                <span class="right-arrow">></span>
+
+
+                <img src="https://cdn.myanimelist.net/images/anime/1168/148347.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1697/151793.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1959/151055.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1140/152364.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1190/151754.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1276/151118.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1206/151772.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1699/151694.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1364/151767.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1830/145051.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1163/151246.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1721/151097.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1011/152084.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1651/152063.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1864/151837.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1015/151233.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1264/152012.jpg">
+                <img src="https://cdn.myanimelist.net/images/anime/1257/152233.jpg">
+            </div>
 
         </div>
         <div class="rightSection">
