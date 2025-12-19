2c2f8ca526083bd8abc9c3489d0cdfdbdd60a74c
Arindom-Lane <arindom793@gmail.com>
2025-12-19 17:07:16 +0600

    fully used CRUD in php using mysql

--- php-DataBase/delete.php
+++ php-DataBase/delete.php
@@ -1,18 +1,13 @@
 <?php
 include("db.php");
 
-if(isset($_POST['delete'])){
-    setcookie('d_Id',$_POST['id'],time()+3600);
-    $id =$_COOKIE['d_ID'];
-
-    $result = mysqli_query($conn,'DELETE FORM applicants WHERE id=$id');
-
-    if($result){
-        header('Location: test.php?deleted=1');
-        exit;
+if(isset($_POST["id"])){
+    $id = $_POST["id"];
+    $query = mysqli_query($con,"DELETE FROM applicants WHERE id=$id");
+    
+    if($query){
+        header("Location: test.php?updated=1");
+        exit();
     }
-    else{
-        echo'Error deleting: '.mysqli_error($con);
-    }
-}
+    else{exit();}}
 ?>
