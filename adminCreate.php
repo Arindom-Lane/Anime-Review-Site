34697925f0a332bf6669b7ad3fb5bb9edbb2f36d
Arindom-Lane <arindom793@gmail.com>
2025-12-25 19:31:18 +0600

    added admins ability to create new movies. add new movie/tshow/manga data to database

--- adminCreate.php
+++ adminCreate.php
@@ -1,7 +1,35 @@
-<?PHP 
+<?php 
+session_start();
 include ("db.php");
 if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true){
     header('Location: login.php');
  }
+
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $title = $_POST["title"];
+    $type = $_POST["type"];
+    $poster = $_POST["poster"];
+    $studio = $_POST["studio"];
+    $producer = $_POST["producer"];
+    $genre = $_POST["genre"];
+    $duration = $_POST["duration"];
+    $source = $_POST["source"];
+    $description = $_POST["description"];
+
+    $query = "INSERT INTO Media (title, description, type, poster_image_link, producer, studio, source, genre, duration) 
+              VALUES ('$title', '$description', '$type', '$poster', '$producer', '$studio', '$source', '$genre', '$duration')";
+
+    if(mysqli_query($conn, $query)){ 
+        header("Location: admin.php?");
+        exit();
+    }
+    else{
+        $_SESSION["CreateError"] = true;
+        header("Location: admin.php");
+        exit();
+    }
+ }
+
+
 
 ?>
