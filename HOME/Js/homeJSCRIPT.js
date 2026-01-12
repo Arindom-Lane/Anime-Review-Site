481a51560a8256c4029599ffe7b97ffcba13b288
Arindom-Lane <arindom793@gmail.com>
2026-01-12 23:46:00 +0600

    dark theme adddedd to home. changed major color form mal

--- HOME/Js/homeJSCRIPT.js
+++ HOME/Js/homeJSCRIPT.js
@@ -60,3 +60,19 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 })
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
