53d2226c75e86fd9d3bd403ed4facc4c4c99a48b
Ayan <169395060+ReDThunDeR33@users.noreply.github.com>
2026-01-12 23:40:45 +0600

    .

--- USER/View/favouriteList.php
+++ USER/View/favouriteList.php
@@ -13,7 +13,8 @@ if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My AnimeList Dashboard</title>
-    <link rel="stylesheet" href="../Css/userDash.css">  
+    <link rel="stylesheet" href="../Css/userDash.css"> 
+    <link rel="stylesheet" href="../../HOME/Css/topAnime-MangaPage.css"> 
     <link rel="stylesheet" href="../../HOME/Css/searchBar.css">  
 </head>
 <body>
