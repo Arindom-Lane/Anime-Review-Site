f8ae91a8dc2ad3318a90eede27f9ddd3f296b2c3
Ayan <169395060+ReDThunDeR33@users.noreply.github.com>
2026-01-15 00:42:42 +0600

    .

---
+++ USER/View/AnimeList.php
@@ -0,0 +1,145 @@
+<?php
+session_start();
+include("../../HOME/Model/db.php");
+
+if (isset($_POST['theme-toggle'])) {
+    $newTheme = $_POST['theme-toggle'] == '1' ? 'dark' : 'light';
+    $_SESSION['theme_mode'] = $newTheme;
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
+    header('Location: ../../HOME/View/login.php');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My AnimeList Dashboard</title>
+    <link rel="stylesheet" href="../Css/userDash.css"> 
+    <link rel="stylesheet" href="../../HOME/Css/topAnime-MangaPage.css"> 
+    <link rel="stylesheet" href="../../HOME/Css/searchBar.css">  
+</head>
+<body class="<?php echo (isset($_SESSION['theme_mode']) && $_SESSION['theme_mode'] === 'dark') ? 'dark-theme' : ''; ?>">
+    <header>
+        <div class="header-upper">
+            <div class="logo" onclick="window.location.href='../../HOME/View/home.php'">
+                <img src="../../HOME/Images/download.png" alt="Logo">
+            </div>
+            <div class="profile">
+                <?php if (isset($_SESSION['username']) && $_SESSION['loggedIn'] === true): ?>
+                <div class="devider1"></div>
+                <span class="profile-name" onclick="window.location.href='userDashboard.php'">
+                    <?php echo $_SESSION['username']; ?>
+                </span>
+                <img src="<?php echo $_SESSION['profileImage']; ?>" alt="Profile" onclick="window.location.href='../../USER/View/userDashboard.php'">
+                <a href="../../HOME/Controler/destorySession.php" class="login-link-Log-out">Log Out</a>
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="header-middle">
+            <div class="topButton">
+                <span>TOP ANIME</span>
+                <span>TOP MANGA</span>
+            </div>
+            <div class="search-bar">
+                <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                <form method="POST">
+                    <input class="search" id="search" type="text" name="search" placeholder="Search...">
+                </form>
+                <div class="search-results" id="search-results">
+                    
+                </div>
+            </div>
+            <script>
+                $(document).ready(function() {
+                    $('#search').on('input', function() {
+                        var query = $(this).val();
+                        if (query.length > 2) {
+                            $.ajax({
+                                url: '../../HOME/Controler/searchBarLogic.php',
+                                method: 'POST',
+                                data: {
+                                    search: query
+                                },
+                                success: function(data) {
+                                    $('#search-results').html(data).show();
+                                }
+                            });
+                        } else {
+                            $('#search-results').hide();
+                        }
+                    });
+                });
+            </script>
+        </div>
+        <div class="header-lower">
+            <span>My Panel</span>
+            <img src="https://cdn-icons-png.freepik.com/512/14911/14911421.png" alt="Menu">
+        </div>
+    </header>
+
+    <main>
+        <!-- LEFT SIDEBAR -->
+        <div class="leftSection">
+            <fieldset class="sidebar-fieldset">
+                <?php if (isset($_SESSION['username']) && $_SESSION['loggedIn'] === true): ?>
+                <div class="user-avatar-container">
+                    <img src="<?php echo $_SESSION['profileImage']; ?>" class="user-avatar-img" alt="User Avatar">
+                </div>
+                <?php endif; ?>
+                
+                <div class="sidebar-content">
+                    <div class="sidebar-row">
+                        <span>Last Online</span>
+                        <span class="status-online">Now</span>
+                    </div>
+                    <div class="sidebar-row joined-row">
+                        <span>Joined</span>
+                        <span>Aug 11, 2023</span>
+                    </div>
+
+
+                    <div class="sidebar-divider"></div>
+                    
+                    <div class="sidebar-menu">
+                        <input type="button" value="Favorites" class="statbtn">
+                    </div>
+
+                    <br>
+                    
+                </div>
+                
+                <div class="edit-profile-wrapper" onclick="window.location.href='UserEditProfile.php'">
+                    <input type="button" value="Edit Profile" class="editbtn">
+                </div>
+            </fieldset>
+        </div>
+
+        <!-- RIGHT MAIN CONTENT -->
+        <div class="rightsection">
+                        <div class="admin-box">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Poster</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    
+            </div> 
+                 
+                
+        </div>
+            
+    </main>
+    
+    <script src="../Js/favouriteList.js"></script>
+    <script src="../Js/homeJSCRIPT.js"></script>
+</body>
+</html>
