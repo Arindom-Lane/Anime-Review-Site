3291678b283778fb23d17c8ada65629e45f396e4
Arindom-Lane <arindom793@gmail.com>
2026-01-19 14:44:59 +0600

    .

--- ADMIN/Controler/adminStatus.php
+++ ADMIN/Controler/adminStatus.php
@@ -3,10 +3,10 @@
 include("../../HOME/Model/db.php");
 
 $data = [
-    "users" => mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM users")),
-    "media" => mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM media")),
-    "tvShows" => mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM media WHERE type = 'tvShow'")),
-    "manga" => mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM media WHERE type = 'manga'")),
+    "users" => mysqli_num_rows(mysqli_query($conn, "SELECT * FROM Users")),
+    "media" => mysqli_num_rows(mysqli_query($conn, "SELECT * FROM Media")),
+    "tvShows" => mysqli_num_rows(mysqli_query($conn, "SELECT * FROM Media WHERE type = 'tvShow'")),
+    "manga" => mysqli_num_rows(mysqli_query($conn, "SELECT * FROM Media WHERE type = 'manga'")),
 ];
 
 header('Content-Type: application/json');
